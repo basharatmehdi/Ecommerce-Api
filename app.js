dbc1b87e1c58b7530afd9c43877c84998b743573
Basharat Mehdi <basharatmehdidar@gmail.com>
2023-03-29 14:00:40 +0530

    some changes in server

--- app.js
+++ app.js
@@ -29,12 +29,12 @@ app.use(cors());
 app.use(helmet());
 app.use(xss());
 app.use(mongoSanitize());
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-);
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // limit each IP to 100 requests per windowMs
+//   })
+// );
 
 // middleware
 app.use(express.json());
